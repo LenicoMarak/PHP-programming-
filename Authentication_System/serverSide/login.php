922a663d87a9d83ed54a51ab134f641142c2d9b7
Lenico Marak <lenicomarak18@gmail.com>
2024-02-03 22:31:38 +0530

    updated

--- Authentication_System/serverSide/login.php
+++ Authentication_System/serverSide/login.php
@@ -1,18 +1,31 @@
-<?php 
-require ("Query.php");
-$number="";
-$password="";
- if(isset($_POST["login"])){
-    $number=$_POST['number'];
-    $password=$_POST['password'];
- }
- $query=new Query();
- if(!empty($number) && !empty($password)){
-   if($query->isExist($number,$password)){
-     echo "Yor have successfully login<br>";
-   }
-   else{
-     echo "<script>alert('Wrong password or number');</script>";
-   }
- }
-?>
+ <html>
+  <head>
+    <meta charset="utf-8"/>
+    <meta name="author" content="Aex Software's"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no"/>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+    <?php 
+    require ("Query.php");
+    $number="";
+    $password="";
+    if(isset($_POST["login"])){
+      $number=$_POST['number'];
+      $password=$_POST['password'];
+    }
+    $query=new Query();
+    if(!empty($number) && !empty($password)){
+      if($query->isExist($number,$password)){
+        echo "Yor have successfully login<br>";
+      }
+      else{?>
+        <script>
+          alert("Wrong password or number ");
+          window.location="../index.html";
+        </script> 
+      <?}
+    }
+    ?>
+  </body>
+</html>
