349816eee55e6dd5258cbba15f3bf5278d154fd9
Lenico Marak <lenicomarak18@gmail.com>
2024-02-03 22:29:08 +0530

    updated

--- Authentication_System/serverSide/Query.php
+++ Authentication_System/serverSide/Query.php
@@ -17,13 +17,14 @@ class Query extends DbConnection{
     parent::connect();
   }
   
-  public function getName($roll){
+  public function getName($number){
+    $data="";
     try{
       $sql="select FirstName from User where Roll_no=?;";
       $st=parent::getConn()->prepare($sql);
-      $st->bindParam(1,$roll,PDO::PARAM_INT);
+      $st->bindParam(1,$number,PDO::PARAM_INT);
       $st->execute();
-      $data=$st->fetch();
+      $data=$st->fetchColumn();
     }
 
     catch(PDOExcepton $e){
@@ -32,7 +33,8 @@ class Query extends DbConnection{
     return $data;
   }
         
-  public function isExist($number,$password){
+  public function isExist($num,$pass){
+    $result=false;
     try{
       $sth=parent::getConn()->prepare('SELECT Number,Password FROM User where Number=? AND Password=?;');
       $sth->bindParam(1,$number,PDO::PARAM_STR);
@@ -41,10 +43,10 @@ class Query extends DbConnection{
       while($data=$sth->fetch(PDO::FETCH_ASSOC)){
       while($data=$sth->fetch(PDO::FETCH_ASSOC)){
       while($data=$sth->fetch(PDO::FETCH_ASSOC)){
-         $numMatch=$data["Number"];
-         $passMatch=$data["Password"];
-         if($numMatch==$number && $passMatch== $password){
-            return true;
+         $number=$data["Number"];
+         $password=$data["Password"];
+         if(strcmp($number,$num)==0 && strcmp($password,$pass)==0){
+            $result=true;
          }
       }
     }
@@ -53,10 +55,11 @@ class Query extends DbConnection{
       var_dump($e->getMessage());
       var_dump($e->errInfo());
     }
-    return false;
+    return $result;
   }
    
   public function insert($firstName,$lastName,$number,$gender,$password){
+    $rows=0;
     try{
       $sql="INSERT INTO User(
                FirstName,
@@ -82,7 +85,6 @@ class Query extends DbConnection{
     }
     return $rows;
   }
-   
   public function __destruct(){
     parent::closeConn();
   }
--- Authentication_System/serverSide/login.php
+++ Authentication_System/serverSide/login.php
@@ -1,14 +1,18 @@
 <?php 
 require ("Query.php");
+$number="";
+$password="";
  if(isset($_POST["login"])){
     $number=$_POST['number'];
     $password=$_POST['password'];
  }
  $query=new Query();
- if($query->isExist($number,$password)){
-   echo "Yor have successfully login<br>";
- }
- else{
-   echo "Looks like you don't have an account!!<br>";
+ if(!empty($number) && !empty($password)){
+   if($query->isExist($number,$password)){
+     echo "Yor have successfully login<br>";
+   }
+   else{
+     echo "<script>alert('Wrong password or number');</script>";
+   }
  }
 ?>
